517db92f966c7542dcc6e7b08b35cc5307bff340
Atul <atulreso1@gmail.com>
2024-01-01 23:40:39 +0530

    add user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,92 @@
-import mongoose from 'mongoose';
-const userSchema = new Schema({}, { timestamps: true });
+import bcrypt from "bcrypt";
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+      // if u want to make searchable field with optimise way then make index as true, so it came on searching in db. without using index is also a way to search in db, but using this is better way. (it can be a little expensive)
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avtar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Pre hook in mongoose: Pre middleware functions are executed one after another, when each middleware calls next.
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// async/await optional
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      // payload_key: db_key
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
